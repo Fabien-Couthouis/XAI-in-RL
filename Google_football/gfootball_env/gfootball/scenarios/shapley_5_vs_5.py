f3fce792e213fb835797e52540e0900b460ad7bf
FABIEN-COUTHOUIS <couthouis.fabien@gmail.com>
2020-05-25 22:21:13 +0200

    Correct team changing in 5 vs 5 scenario

--- Google_football/gfootball_env/gfootball/scenarios/shapley_5_vs_5.py
+++ Google_football/gfootball_env/gfootball/scenarios/shapley_5_vs_5.py
@@ -19,12 +19,12 @@ def build_scenario(builder):
     builder.config().game_duration = 1500
     builder.config().right_team_difficulty = 0.05
     builder.config().deterministic = False
-    if builder.EpisodeNumber() % 2 == 0:
-        first_team = Team.e_Left
-        second_team = Team.e_Right
-    else:
-        first_team = Team.e_Right
-        second_team = Team.e_Left
+    # if builder.EpisodeNumber() % 2 == 0:
+    first_team = Team.e_Left
+    second_team = Team.e_Right
+    # else:
+    #     first_team = Team.e_Right
+    #     second_team = Team.e_Left
     builder.SetTeam(first_team)
     builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
     builder.AddPlayer(0.000000,  0.020000, e_PlayerRole_RM)
