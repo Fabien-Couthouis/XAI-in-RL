76c925c27b08b1a27999de05dd78e11dfdb29fb0
FABIEN-COUTHOUIS <couthouis.fabien@gmail.com>
2020-05-26 23:06:14 +0200

    Fix shapley_3_vs_2 scenario training
    Add goalkeeper

--- Google_football/train.sh
+++ Google_football/train.sh
@@ -1,1 +1,1 @@
-python train.py --scenario-name "shapley_5_vs_5" --num-agents 5 --num-gpu 0 --num-iters 10 --run "MADDPG"
+python train.py --scenario-name "shapley_3_vs_2" --num-agents 3 --num-gpu 0 --num-iters 100  --run "PPO"
