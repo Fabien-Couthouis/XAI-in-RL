2034f354ecf786490300da70b98f279aaf906d0e
Fabien Couthouis <couthouis.fabien@gmail.com>
2021-01-24 22:28:37 +0100

    Experiments for speed plot (particles)

--- multiagent_particles/experiments/launch_experiments.py
+++ multiagent_particles/experiments/launch_experiments.py
@@ -10,13 +10,14 @@ def save_rewards(N, M, folder_name, agent_speeds):
     for n in range(N):
         for run in runs:
             for behaviour in missing_agents_behaviours:
-                fname = f"{folder_name}/{run}_{behaviour}_{n}.csv"
-                if not os.path.exists(fname):
-                    print(fname)
-                    command = f'python run.py --load-dir "saves/{run}/episode_200000/model" --missing-agents-behaviour {behaviour} --exp-name {run}_{behaviour}_{n} --save-dir {folder_name} --shapley-M {M} --num-episodes 1 --agent-speeds'
-                    for speed in agent_speeds:
-                        command += f" {speed}"
-                    subprocess.run(command, shell=True)
+                for speeds in agent_speeds:
+                    fname = f"{folder_name}/{run}_{behaviour}_{speeds}_{n}.csv"
+                    if not os.path.exists(fname):
+                        print(fname)
+                        command = f'python run.py --load-dir "saves/{run}/episode_200000/model" --missing-agents-behaviour {behaviour} --exp-name {run}_{behaviour}_{n} --save-dir {folder_name} --shapley-M {M} --num-episodes 1 --agent-speeds'
+                        for speed in speeds:
+                            command += f" {speed}"
+                        subprocess.run(command, shell=True)
 
 
 def save_rewards_true_shapley(N, n_episodes, folder_name, agent_speeds):
@@ -25,13 +26,14 @@ def save_rewards_true_shapley(N, n_episodes, folder_name, agent_speeds):
     for n in range(N):
         for run in runs:
             for behaviour in missing_agents_behaviours:
-                fname = f"{folder_name}/{run}_{behaviour}_{n}.csv"
-                if not os.path.exists(fname):
-                    print(fname)
-                    command = f'python run.py --load-dir "saves/{run}/episode_200000/model" --missing-agents-behaviour {behaviour} --exp-name {run}_{behaviour}_{n} --true-shapley --save-dir {folder_name} --num-episodes {n_episodes} --agent-speeds'
-                    for speed in agent_speeds:
-                        command += f" {speed}"
-                    subprocess.run(command, shell=True)
+                for speeds in agent_speeds:
+                    fname = f"{folder_name}/{run}_{behaviour}_{speeds}_{n}.csv"
+                    if not os.path.exists(fname):
+                        print(fname)
+                        command = f'python run.py --load-dir "saves/{run}/episode_200000/model" --missing-agents-behaviour {behaviour} --exp-name {run}_{behaviour}_{n} --true-shapley --save-dir {folder_name} --num-episodes {n_episodes} --agent-speeds'
+                        for speed in speeds:
+                            command += f" {speed}"
+                        subprocess.run(command, shell=True)
 
 
 def save_goal_agents(N, folder_name, agent_speeds):
@@ -47,11 +49,14 @@ def save_goal_agents(N, folder_name, agent_speeds):
 
 
 if __name__ == "__main__":
-    SPEEDS_EXP1 = [1.0, 1.0, 1.0, 1.3]
-    SPEEDS_EXP2 = [0.2, 0.8, 2.0, 1.3]
-    # save_rewards(N=1, M=1000, folder_name="rewards/exp1",
-    #              agent_speeds=SPEEDS_EXP2)
-    # save_goal_agents(2000, "goal_agents/exp1",
-    #                  agent_speeds=SPEEDS_EXP1)
-    save_rewards_true_shapley(N=1, n_episodes=1000, folder_name="rewards/true-shap-exp1",
-                              agent_speeds=SPEEDS_EXP1)
+    # SPEEDS_EXP1 = [[1.0, 1.0, 1.0, 1.3]] #default
+    SPEEDS = []
+    for speed_a1 in range(0, 22, 2):
+        SPEEDS.append([speed_a1/10, 1.0, 1.0, 1.3])
+
+    save_rewards(N=1, M=500, folder_name="rewards/true-shap-exp2-speeds-chart",
+                 agent_speeds=SPEEDS)
+    # # save_goal_agents(2000, "goal_agents/exp1",
+    # #                  agent_speeds=SPEEDS_EXP1)
+    # save_rewards_true_shapley(N=1, n_episodes=1000, folder_name="rewards/true-shap-exp22",
+    #                           agent_speeds=SPEEDS_EXP2)
