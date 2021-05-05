d8f61a03d0e3feda1915c201bb7f0a0c08d40f2a
Alexandre Heuillet <ale.heuillet@gmail.com>
2021-05-05 16:21:48 +0200

    added observation for repeated game

--- cooperative_prisoners_dilemma/env.py
+++ cooperative_prisoners_dilemma/env.py
@@ -1,10 +1,10 @@
 import gym
-from gym.spaces import Discrete
+from gym.spaces import Discrete, Box
 
 
 class PrisonerDilemma(gym.Env):
 
-    def __init__(self) -> None:
+    def __init__(self, num_repetitions) -> None:
         super().__init__()
         self.action_space = Discrete(4)  #0: Cooperate with signal "cooperate", 1: Cooperate with signal "defect", 2: Defect with signal "cooperate", 3: Defect with signal 'defect"
-        self.observation_space = None
+        self.observation_space = Box(low=-2*num_repetitions, high=6*num_repetitions, shape=(), dtype=int) #Obs is the sum of total payouts for all past stage games
