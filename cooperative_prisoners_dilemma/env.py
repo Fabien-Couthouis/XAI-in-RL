99a4f215c9f6ff694af7595e580f1e60a2daebc6
Alexandre Heuillet <ale.heuillet@gmail.com>
2021-05-10 10:59:24 +0200

    replaced multidiscrete by discrete in prisoner's dilemma

--- cooperative_prisoners_dilemma/env.py
+++ cooperative_prisoners_dilemma/env.py
@@ -1,7 +1,10 @@
 from gym.spaces import Discrete, Box, Dict, MultiDiscrete
+from numpy.testing._private.utils import decorate_methods
 from ray.rllib.env import MultiAgentEnv
 import numpy as np
 from prettytable import PrettyTable
+
+ACTION_MAP = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2]]
 
 class PrisonerDilemma(MultiAgentEnv):
 
@@ -9,7 +12,7 @@ class PrisonerDilemma(MultiAgentEnv):
         super().__init__()
         self.num_players = env_config["num_players"]
         self.num_repetitions = env_config["num_repetitions"]
-        self.action_space = MultiDiscrete([2, 3])  #0: Cooperate with signal "cooperate", 1: Cooperate with signal "defect", 2: Cooperate with no signal, 3: Defect with signal "cooperate", 4: Defect with signal 'defect", 5: Defect with no signal
+        self.action_space = Discrete(6)  #0: Cooperate with signal "cooperate", 1: Cooperate with signal "defect", 2: Cooperate with no signal, 3: Defect with signal "cooperate", 4: Defect with signal 'defect", 5: Defect with no signal
         self.observation_space = Dict({"total_payout": Box(low=0, high=3*self.num_players*self.num_repetitions, shape=(), dtype=int), "signals": MultiDiscrete([3]*self.num_players)})
         self.last_actions = {}
         self.last_rewards = {} 
@@ -24,11 +27,12 @@ class PrisonerDilemma(MultiAgentEnv):
     
     def render(self, mode):
         agent_table = PrettyTable()
-        act_map = ["Cooperate", "Defect"]
+        loc_map = ["Cooperate", "Defect"]
         agent_table.field_names = ["Agent id", "Action", "Signal", "Reward"]
 
         for agent_id, action in self.last_actions:
-            agent_table.add_row([agent_id, act_map[action[0]], action[1], self.last_rewards[agent_id]])
+            a, s = ACTION_MAP[action]
+            agent_table.add_row([agent_id, loc_map[a], s, self.last_rewards[agent_id]])
 
         info_table = PrettyTable()
         info_table.field_names = ["Total Payout", "Current Stage"]
@@ -38,20 +42,29 @@ class PrisonerDilemma(MultiAgentEnv):
         print(info_table)
         return super().render(mode=mode)
     
+    def decouple_actions_signals(self, actions):
+        acts = []
+        sigs = []
+        for agent_id, action in actions.items():
+            a, s = ACTION_MAP[action]
+            acts.append(a)
+            sigs.append(s)
+        return acts, sigs
+    
     def step(self, actions):
-        act_array = np.array(actions.values())
-        num_coop = np.sum(np.dstack(act_array)[0, 0])
+        acts, sigs = self.decouple_actions_signals(actions)
+        num_coop = np.sum(acts)
         rewards = {}
         if num_coop == 0:
             rewards = {f"agent_{i}": 1 for i in range(self.num_players)}
         elif num_coop == self.num_players:
             rewards = {f"agent_{i}": 3 for i in range(self.num_players)}
         else:
-            for agent_id, a in actions.items():
+            for i, a in enumerate(acts):
                 r = 5 if a[0] == 1 else 0
-                rewards[agent_id] = a
+                rewards[f"agent_{i}"] = r
         self.total_payout += np.sum(rewards)
-        ob = {"total_payout": self.total_payout, "signals": np.dstack(act_array)[0,1]}
+        ob = {"total_payout": self.total_payout, "signals": sigs}
         done = (self.current_stage == self.num_repetitions)
         obs = {}
         dones = {}
